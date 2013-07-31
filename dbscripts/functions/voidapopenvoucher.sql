ee840242668b9317ca53edb8cebe33759abf68fa
garyhgohoos <gary@xtuple.com>
2013-07-31 14:45:57 -0400

    Issue #20561:create C/M with doc date same as glDate

--- dbscripts/functions/voidapopenvoucher.sql
+++ dbscripts/functions/voidapopenvoucher.sql
@@ -298,7 +298,7 @@ BEGIN
     apopen_amount, apopen_paid, apopen_open, apopen_notes, apopen_discount, apopen_curr_rate )
   SELECT _apopenid, getEffectiveXtUser(), pJournalnumber,
          apopen_vend_id, apopen_docnumber, 'C', apopen_ponumber,
-         CURRENT_DATE, CURRENT_DATE, CURRENT_DATE, -1, apopen_curr_id,
+         _glDate, _glDate, _glDate, -1, apopen_curr_id,
          apopen_amount - apopen_paid, 0, TRUE, _reference, TRUE, apopen_curr_rate
     FROM apopen
    WHERE (apopen_id=_n.apopen_id);
