6a4f9261b37b2157a6f1540ce7f0d5e497edd4cf
Greg Pazo <gpazo@xtuple.com>
2013-08-23 14:27:04 -0400

    remove BOM characters

--- updatescripts/from_420alpha/alterPrj.sql
+++ updatescripts/from_420alpha/alterPrj.sql
@@ -1,4 +1,4 @@
-﻿ALTER TABLE prj ADD COLUMN prj_prjtype_id integer,
+ALTER TABLE prj ADD COLUMN prj_prjtype_id integer,
   ADD CONSTRAINT prj_prj_prjtype_id_fkey FOREIGN KEY (prj_prjtype_id)
       REFERENCES prjtype (prjtype_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION;
