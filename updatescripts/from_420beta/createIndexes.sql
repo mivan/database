e819b516986dbd56a9cf49a063c80dcc3bf23a16
garyhgohoos <gary@xtuple.com>
2013-09-11 10:04:27 -0400

    Issue #21064:create 2 indexes on cashrcptitem

---
+++ updatescripts/from_420beta/createIndexes.sql
@@ -0,0 +1,2 @@
+create index cashrcptitem_cashrcpt_idx on cashrcptitem using btree (cashrcptitem_cashrcpt_id);
+create index cashrcptitem_aropen_idx on cashrcptitem using btree (cashrcptitem_aropen_id);
