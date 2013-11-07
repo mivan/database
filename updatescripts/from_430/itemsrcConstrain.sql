e212463e1df41de59fcfa0bd6511d8e9efd4b042
Alfredo Martinez <alfredo@solucionesopen.com.mx>
2013-11-07 15:02:42 -0600

    Moving itemsrc constrain update

---
+++ updatescripts/from_430/itemsrcConstrain.sql
@@ -0,0 +1,4 @@
+ALTER TABLE itemsrc DROP CONSTRAINT itemsrc_itemsrc_vend_id_key;
+
+ALTER TABLE itemsrc
+  ADD CONSTRAINT itemsrc_itemsrc_vend_id_key UNIQUE(itemsrc_vend_id , itemsrc_item_id , itemsrc_effective , itemsrc_expires , itemsrc_vend_item_number , itemsrc_manuf_name , itemsrc_manuf_item_number , itemsrc_contrct_id );
