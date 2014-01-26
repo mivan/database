ef128340cff8d79d76cb6e05dfd409d4950c7835
John Rogelstad <john@xtuple.com>
2014-01-26 08:23:13 -0500

    Improve performance.

--- dbscripts/functions/stdcost.sql
+++ dbscripts/functions/stdcost.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION stdCost(INTEGER) RETURNS NUMERIC AS '
+CREATE OR REPLACE FUNCTION stdCost(INTEGER) RETURNS NUMERIC STABLE AS '
 -- Copyright (c) 1999-2012 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 BEGIN
@@ -6,7 +6,7 @@ BEGIN
 END;
 ' LANGUAGE 'plpgsql';
 
-CREATE OR REPLACE FUNCTION stdCost(INTEGER, INTEGER) RETURNS NUMERIC AS '
+CREATE OR REPLACE FUNCTION stdCost(INTEGER, INTEGER) RETURNS NUMERIC STABLE AS '
 -- Copyright (c) 1999-2012 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
